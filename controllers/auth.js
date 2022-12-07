a4d3ecaa1e306856b4a4fcf66493ab096ec14546
martinManzanares <martin.rivera@potros.itson.edu.mx>
2022-12-06 22:40:58 -0700

    modified:   .env modified:   controllers/auth.js new file:   helpers/google-verify.js modified:   package-lock.json modified:   package.json modified:   public/index.html modified:   routes/auth.js

--- controllers/auth.js
+++ controllers/auth.js
@@ -1,9 +1,11 @@
-const { response } = require("express");
+const { response, json } = require("express");
 const bcryptjs = require('bcryptjs');
 
 
 const Usuario = require('../models/usuario');
+
 const { generarJWT } = require("../helpers/generar-jwt");
+const { googleVerify } = require("../helpers/google-verify");
 
 const login = async (req, res = response) => {
 
@@ -33,7 +35,6 @@ const login = async (req, res = response) => {
         // Generar el JWT.
         const token = await generarJWT( usuario.id );
         
-
         res.json({
             usuario,
             token
@@ -46,7 +47,50 @@ const login = async (req, res = response) => {
     } 
 }
 
+const googleSignIn = async (req, res = response) => {
+    
+    const { id_token } = req.body;
+
+    try {
+        const { correo, nombre, img } = await googleVerify( id_token );
+        
+        let usuario = await Usuario.findOne({ correo });
+        if ( !usuario ) {
+            //Tengo que crearlo.
+            const data = {
+                nombre, 
+                correo,
+                password: ':P',
+                img,
+                google: true
+            };
+
+            usuario = new Usuario( data );
+            await usuario.save();
+        }
+        // Si el usuario en DB su estado: false
+        if ( !usuario.estado ) {
+            return res.status(401).json({
+                msg: 'Hable con el administrador, usuario bloqueado'
+            });
+        }
+        // Generar JWT.
+        const token = await generarJWT( usuario.id );
+
+        res.json({
+            usuario,
+            token
+        });
+
+    } catch (error) {
+        res.status(400).json({
+            msg: 'El token no se pudo verificar'
+        }); 
+    }
+}
+
 
 module.exports = {
-    login
+    login,
+    googleSignIn
 }
--- routes/auth.js
+++ routes/auth.js
@@ -2,7 +2,7 @@ const { Router } = require('express');
 const { check } = require('express-validator');
 
 const { validarCampos } = require('../middlewares/validar-campos');
-const { login } = require('../controllers/auth');
+const { login, googleSignIn } = require('../controllers/auth');
 
 const router = Router();
 
@@ -12,6 +12,11 @@ router.post('/login',[
     check('password', 'la contraseña es obligatoria').not().isEmpty(),
     validarCampos
 ], login);
+
+router.post('/google',[
+    check('id_token', 'id_token es necesario').not().isEmpty(),
+    validarCampos
+], googleSignIn);
 
 
 module.exports = router;
